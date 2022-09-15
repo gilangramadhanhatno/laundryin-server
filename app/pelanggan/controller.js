a2fcfd8baffdf5041f991f9b62ce027374370248
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2022-09-15 16:55:07 +0700

    crud pelanggan

--- app/pelanggan/controller.js
+++ app/pelanggan/controller.js
@@ -1,9 +1,73 @@
+const Pelanggan = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
+      const pelanggan = await Pelanggan.find();
       res.render("admin/pelanggan/view_pelanggan", {
-        title: "Laundryin",
+        pelanggan,
       });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/pelanggan/tambah_pelanggan");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name, tel, address } = req.body;
+      let pelanggan = await Pelanggan({ name, tel, address });
+
+      await pelanggan.save();
+
+      res.redirect("/pelanggan");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const pelanggan = await Pelanggan.findOne({ _id: id });
+
+      res.render("admin/pelanggan/ubah_pelanggan", {
+        pelanggan,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, tel, address } = req.body;
+
+      await Pelanggan.findByIdAndUpdate(
+        {
+          _id: id,
+        },
+        { name, tel, address }
+      );
+
+      res.redirect("/pelanggan");
+    } catch (error) {
+      console.log(erro);
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      await Pelanggan.findOneAndRemove({
+        _id: id,
+      });
+
+      res.redirect("/pelanggan");
     } catch (error) {
       console.log(error);
     }
---
+++ app/pelanggan/model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+let pelangganSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Nama pelanggan harus diisi"],
+  },
+  tel: {
+    type: String,
+    // min: [6, "No telepon terlalu sedikit"],
+    // max: 12,
+    require: [true, "Nomor telepon harus diisi"],
+  },
+  address: {
+    type: String,
+    require: [true, "Alamat harus diisi"],
+  },
+});
+
+module.exports = mongoose.model("Pelanggan", pelangganSchema);
